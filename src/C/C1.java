11b895e458040623c8e52b2ad229bcc8b52ce924
Kashumuto <tynchyev.baktiyar@gmail.com>
2024-02-11 17:45:42 +0600

    C1-commit-3

--- src/C/C1.java
+++ src/C/C1.java
@@ -4,4 +4,8 @@ public class C1 {
     public static void main(String[] args) {
         System.out.println("Hi there!");
     }
+
+    public static void sayHello() {
+        System.out.println("hello!");
+    }
 }
